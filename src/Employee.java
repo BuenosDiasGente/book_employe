e1a9b0d6e368bac61b9bf1751d43735fb9f92494
BuenosDiasGente <morkovka03121981@gmail.com>
2023-09-20 22:39:10 +0300

    Basic difficulty

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,63 @@
-package PACKAGE_NAME;public class Employee {
+import java.util.Objects;
+
+public class Employee {
+    private int id;
+    private String name;
+    private int department;
+    private double salary;
+    static int count = 1;
+
+    public Employee( String name, int department, double salary) {
+        this.id = count++;
+        this.name = name;
+        this.department = department;
+        this.salary = salary;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", department=" + department +
+                ", salary=" + salary +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return id == employee.id && department == employee.department && Double.compare(employee.salary, salary) == 0 && Objects.equals(name, employee.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, department, salary);
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,92 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Employee[] employee = new Employee[10];
+        for (int i = 0; i < employee.length; i++) {
+            employee[i] = new Employee("Petrov S.V." + i, 1 + (i % 5), 10000 + (i * 100));
+        }
+
+        printEmployees(employee);
+        System.out.println();
+        sumSalaryByMonth(employee);
+        System.out.println();
+        Employee minSalary = getMinSalary(employee);
+        System.out.println("Сотрудник с минимальной зарплатой" + minSalary);
+        System.out.println();
+        Employee maxSalary = getMaxSalary(employee);
+        System.out.println("Сотрудник с максимальной зарплатой" + maxSalary);
+        System.out.println();
+        averageSalary(employee);
+        System.out.println();
+        System.out.println("Ф. И. О. всех сотрудников: ");
+        fullNameByEmployees(employee);
+    }
+
+    /**
+     * The method gets a list of all employees with all the data available on them .
+     */
+    public static void printEmployees(Employee[] employee) {
+        for (Employee employees : employee) {
+            System.out.println(employees);
+        }
+
+    }
+
+    /**
+     * The method calculates the amount of salary costs per month.
+     */
+    public static void sumSalaryByMonth(Employee[] employees) {
+        double result = 0;
+        for (Employee employee : employees)
+            result += employee.getSalary();
+        System.out.println("Сумму затрат на зарплаты в месяц " + result);
+    }
+
+    /**
+     * The method finds an employee with a minimum salary.
+     */
+    public static Employee getMinSalary(Employee[] employees) {
+        Employee result = employees[0];
+        for (Employee employee : employees) {
+            if (result.getSalary() < employee.getSalary()) {
+            }
+        }
+        return result;
+    }
+
+    /**
+     * The method finds the employee with the maximum salary.
+     */
+    public static Employee getMaxSalary(Employee[] employees) {
+        Employee result = employees[0];
+        for (Employee value : employees) {
+            if (result.getSalary() > value.getSalary()) {
+            }
+        }
+        return result;
+    }
+
+    /**
+     * The method calculates the average value of salaries.
+     */
+    public static void averageSalary(Employee[] employees) {
+        double result = 0;
+        for (int i = 0; i < employees.length; i++)
+            result += employees[i].getSalary() / employees.length;
+        System.out.println("Среднее значение затрат на зарплаты в месяц " + result);
+    }
+
+    /**
+     * The method gets the full name of all employees.
+     */
+    public static void fullNameByEmployees(Employee[] employees) {
+        for (Employee employee : employees) {
+            String name = employee.getName();
+            System.out.print(" " + name + " ");
+        }
     }
 }
+
+
